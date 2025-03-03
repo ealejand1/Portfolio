1d89c4885ccf72c2961ffe5787259e43513b8689
Erik Alejandro Quezada <erikalejjandro1112@gmail.com>
2025-03-03 19:13:26 +0100

    cambio en tecnologias y creacion componente tecnologia

--- src/app/componentes/cuerpo/cuerpo.component.ts
+++ src/app/componentes/cuerpo/cuerpo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProyectoComponent } from './proyecto/proyecto.component';
+import { TecnologiaComponent } from "./tecnologia/tecnologia.component";
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [ProyectoComponent],
+  imports: [ProyectoComponent, TecnologiaComponent],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
---
+++ src/app/componentes/cuerpo/tecnologia/tecnologia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologia',
+  standalone: true,
+  imports: [],
+  templateUrl: './tecnologia.component.html',
+  styleUrl: './tecnologia.component.css'
+})
+export class TecnologiaComponent {
+
+}
